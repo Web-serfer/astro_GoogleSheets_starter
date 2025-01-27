6e023f126fb781deab69d096990336454c8e90b0
RediBedi <redibedi2019@gmail.com>
2025-01-27 12:39:12 +0500

    first commit

---
+++ src/data/lawyers.js
@@ -0,0 +1,26 @@
+export const lawyers = [
+  {
+    id: 1,
+    name: "Иван Иванов",
+    specialization: "Уголовное право",
+    experience: "10 лет",
+    description: "Опытный адвокат по уголовным делам. Защищает интересы клиентов на высшем уровне.",
+    image: "/images/lawyer1.jpg", 
+  },
+  {
+    id: 2,
+    name: "Мария Петрова",
+    specialization: "Семейное право",
+    experience: "8 лет",
+    description: "Эксперт в области семейного права. Помогает решать сложные вопросы разводов и опеки.",
+    image: "/images/lawyer2.jpg",
+  },
+  {
+    id: 3,
+    name: "Алексей Сидоров",
+    specialization: "Корпоративное право",
+    experience: "12 лет",
+    description: "Профессионал в корпоративном праве. Консультирует крупные компании.",
+    image: "/images/lawyer3.jpg",
+  },
+];
